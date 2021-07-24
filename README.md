ae9d6cdd89cea5871c70eecbe86db1efc2bab9e9
shultztom <shultzthomask@sau.edu>
2021-07-24 14:24:39 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# password-generator
+
+## Getting Started
+
+1. Install Deps with `yarn install`
+
+- If yarn is not installed, install it with `npm i -g yarn`
+
+## Usage
+
+- Create password with length of 12
+
+```
+yarn start
+```
+
+- Create password with custom length
+
+```
+yarn start --length=20
+```
---
+++ index.js
@@ -0,0 +1,23 @@
+const {program} = require('commander');
+const crypto = require('crypto');
+
+program.version('1.0.0');
+
+program
+    .option('--length <length>', "Length of password")
+    .parse();
+
+const options = program.opts();
+
+let length = Number(options.length) || 12;
+if(length <= 0){
+    length = 12;
+}
+
+const wishlist = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~!@-#$';
+
+const password = Array.from(crypto.randomFillSync(new Uint32Array(length)))
+    .map((x) => wishlist[x % wishlist.length])
+    .join("");
+
+console.log(password);
